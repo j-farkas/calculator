16b01190ccf37ae496c07ab422eb1bf2fb5c8de1
Jared Farkas and Justin Haupt <justin@>
2019-01-16 15:11:58 -0800

    fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,19 @@ function divide(number1, number2)
 $(document).ready(function() {
   inp = 0;
   var first = 0;
-  var op=null;;
+  var op=null;
+  var dec = false;
+  var count = 0;
   $("#display").text(inp);
   $(".n").click(function() {
-    if($("#display").text().charAt($("#display").text().length-1) === '.'){
-      inp += parseFloat($(this).text()/10);
+    if(dec === true){
+      count +=1;
+      inp += parseFloat($(this).text()/(10**count));
       $("#display").text(inp);
     }else{
       inp *= 10;
       inp += parseFloat($(this).text());
+
       $("#display").text(inp);
     }
   });
@@ -39,6 +43,8 @@ $(document).ready(function() {
     $("#op").text("");
     $("#first").text("");
     op=null;
+    dec = false;
+    count = 0;
   });
   $("#neg").click(function() {
     inp *= -1;
@@ -49,7 +55,10 @@ $(document).ready(function() {
     $("#display").text(inp);
   });
   $("#dcm").click(function() {
+    if(dec === false){
+          dec = true;
     $("#display").text(inp + '.');
+  }
   });
   $(".op").click(function() {
     if(op !== null)
@@ -73,13 +82,16 @@ $(document).ready(function() {
       $("#op").text("");
       $("#first").text("");
       op=null;
+      dec = false;
+      count = 0;
     }
        first = inp;
       $("#first").text(inp);
       $("#op").text($(this).text());
       op = $(this).attr('id');
-      console.log(op);
       inp = 0;
+      dec = false;
+      count = 0;
       $("#display").text(inp);
   });
   $("#eql").click(function() {
@@ -102,6 +114,8 @@ $(document).ready(function() {
     $("#op").text("");
     $("#first").text("");
     op=null;
+    dec = false;
+    count = 0;
   });
 
 });
